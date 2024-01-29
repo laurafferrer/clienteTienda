96edec83cc0402e4fac2614030999619a5cba8ab
laurafferrer <a073667569h@ausiasmarch.net>
2024-01-29 19:34:31 +0100

    Creados components de shared.

---
+++ src/app/components/shared/footer-routed/footer-routed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-routed',
+  templateUrl: './footer-routed.component.html',
+  styleUrls: ['./footer-routed.component.css']
+})
+export class FooterRoutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
