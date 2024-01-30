85a6159054bf4c086bd117dcdd5879b627a6c46f
laurafferrer <a073667569h@ausiasmarch.net>
2024-01-30 19:32:46 +0100

    Añadidas variables de colores.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: '#0B3B59', //Azul
+        dark: '#260808', //Rojo vino
+        darkRed: '#731D1D', //Rosa oscuro
+        red: '#A62E2E', //Rosa 
+        lightRed: '#F25C5C', //Rosa claro
+      }
+    },
   },
   plugins: [],
 }
