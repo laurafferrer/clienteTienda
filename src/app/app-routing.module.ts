5789f186df103f534d13e7d302e7b90ce8bfb12a
laurafferrer <a073667569h@ausiasmarch.net>
2024-01-30 19:51:48 +0100

    Añadidp paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // --
+import { HomeRoutedComponent } from './components/shared/home-routed/home-routed.component';
+import { LoginRoutedComponent } from './components/shared/login-routed/login-routed.component';
+import { LogoutRoutedComponent } from './components/shared/logout-routed/logout-routed.component';
+
 
 const routes: Routes = [
+  { path: '', component: HomeRoutedComponent },
+  { path: 'home', component: HomeRoutedComponent },
+  { path: 'login', component: LoginRoutedComponent},
+  { path: 'logout', component: LogoutRoutedComponent},
+  //{ path: '/admin/user/plist', component: }
 ];
 
 @NgModule({
