49cbe27d98fda8f826880e58d00aa394854aa635
laurafferrer <a073667569h@ausiasmarch.net>
2024-01-31 16:58:16 +0100

    Instalado snack-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSnackBar } from '@angular/material/snack-bar';
 // --
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -52,6 +53,7 @@ import { UserAjaxService } from './service/user.ajax.service';
     CommonModule,
   ],
   providers: [
+    MatSnackBar,
     // SERVICES
     CartAjaxService,
     CategoryAjaxService,
