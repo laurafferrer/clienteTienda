fccee039fd2adba18902d3d86ab495a339c50cec
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-01 21:26:45 +0100

    Component user selection hecho.

--- src/app/components/user/admin-user-selection-unrouted-component/admin-user-selection-unrouted-component.component.ts
+++ src/app/components/user/admin-user-selection-unrouted-component/admin-user-selection-unrouted-component.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser, IUserPage } from '../../../model/model.interfaces';
+import { Paginator, PaginatorState } from 'primeng/paginator';
+import { HttpErrorResponse } from '@angular/common/http';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { UserAjaxService } from '../../../service/user.ajax.service';
 
 @Component({
   selector: 'app-admin-user-selection-unrouted-component',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminUserSelectionUnroutedComponentComponent implements OnInit {
 
-  constructor() { }
+  oPage: IUserPage | undefined;
+  oOrderField: string = "id";
+  oOrderDirection: string = "asc";
+  oPaginatorState: PaginatorState = { first: 0, rows: 10, page: 0, pageCount: 0};
+  oStatus: HttpErrorResponse | null = null;
+  oUsuarioDoRemove: IUser | null = null;
+
+  constructor(
+    private oUserAjaxService: UserAjaxService,
+    public oDialogService: DialogService,
+    public oDynamicDialogRef: DynamicDialogRef
+  ) { }
 
   ngOnInit() {
+    this.getPage();
+  }
+
+  getPage(): void {
+    this.oUserAjaxService.getUserPage(this.oPaginatorState.page || 0, this.oPaginatorState.rows || 0,  this.oOrderField, this.oOrderDirection).subscribe({
+      next: (data: IUserPage) => {
+        this.oPage = data;
+        this.oPaginatorState.pageCount = data.totalPages;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.oStatus = error;
+      }
+    });
+  }
+
+  onPageChange(event: PaginatorState) {
+    this.oPaginatorState.rows = event.rows;
+    this.oPaginatorState.page = event.page;
+    this.getPage();
+  }
+
+  doOrder(orderField: string) {
+    this.oOrderField = orderField;
+    if (this.oOrderDirection == "asc") {
+      this.oOrderDirection = "desc";
+    } else {
+      this.oOrderDirection = "asc";
+    }
+    this.getPage();
+  }
+
+  onSelectUser(oUser: IUser) {
+    this.oDynamicDialogRef.close(oUser);
   }
 
 }
