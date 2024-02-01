4c589aaf8808b25376511b510832f5cac9e2dcca
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-01 19:43:50 +0100

    Añadidas rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // --
 import { HomeRoutedComponent } from './components/shared/home-routed/home-routed.component';
 import { LoginRoutedComponent } from './components/shared/login-routed/login-routed.component';
 import { LogoutRoutedComponent } from './components/shared/logout-routed/logout-routed.component';
+// -- ADMIN COMPONENTS
+import { AdminUserPlistRoutedComponent } from './components/user/admin-user-plist-routed/admin-user-plist-routed.component';
+import { AdminUserEditRoutedComponent } from './components/user/admin-user-edit-routed/admin-user-edit-routed.component';
+import { AdminUserNewRoutedComponent } from './components/user/admin-user-new-routed/admin-user-new-routed.component';
+
 
 
 const routes: Routes = [
@@ -12,7 +16,10 @@ const routes: Routes = [
   { path: 'home', component: HomeRoutedComponent },
   { path: 'login', component: LoginRoutedComponent},
   { path: 'logout', component: LogoutRoutedComponent},
-  //{ path: '/admin/user/plist', component: }
+  
+  { path: 'admin/user/plist', component: AdminUserPlistRoutedComponent},
+  { path: 'admin/user/edit/:id', component: AdminUserEditRoutedComponent},
+  { path: 'admin/user/new', component: AdminUserNewRoutedComponent},
 ];
 
 @NgModule({
