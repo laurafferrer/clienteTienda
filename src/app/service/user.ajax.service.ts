4838d13c61bab73d32e17bb6ec126bbc765d073f
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-01 19:35:35 +0100

    Cambio por errores.

--- src/app/service/user.ajax.service.ts
+++ src/app/service/user.ajax.service.ts
@@ -11,50 +11,59 @@ export class UserAjaxService {
 
   url: string = API_URL + '/user';
 
-  constructor(private http: HttpClient) { }
+  constructor(private oHttpClient: HttpClient) { }
 
   getUserById(id: number) {
-    return this.http.get<IUser>(this.url + '/' + id);
+    return this.oHttpClient.get<IUser>(this.url + '/' + id);
   }
 
   getUserByUsername(username: string): Observable<IUser> {
-    return this.http.get<IUser>(this.url + '/username/' + username);
+    return this.oHttpClient.get<IUser>(this.url + '/username/' + username);
   }
 
   getUserRandom(): Observable<IUser> {
-    return this.http.get<IUser>(this.url + '/random');
+    return this.oHttpClient.get<IUser>(this.url + '/random');
   }
 
-  getUserPage(page: number, size: number, sort: string, direction: string): Observable<IUserPage> {
-    return this.http.get<IUserPage>(this.url + '?page=' + page + '&size=' + size + '&sort=' + sort + ',' + direction);
+  getUserPage(page: number | undefined, size: number | undefined, orderField: string, orderDirection: string, strFilter?: string): Observable<IUserPage> {
+    let sUrl_filter: string;
+    if (!size) size = 10;
+    if (!page) page = 0;
+    if (strFilter && strFilter.trim().length > 0) {
+      sUrl_filter = `&filter=${strFilter}`;
+    } else {
+      sUrl_filter = '';
+    }
+    return this.oHttpClient.get<IUserPage>(this.url + "?size=" + size + "&page=" + page + "&sort" + orderField + "," + orderDirection + sUrl_filter);
+
   }
 
   getUsersByPurchaseDetailDesc(page: number, size: number): Observable<IUserPage> {
-    return this.http.get<IUserPage>(this.url + '/purchaseDetailDesc?page=' + page + '&size=' + size);
+    return this.oHttpClient.get<IUserPage>(this.url + '/purchaseDetailDesc?page=' + page + '&size=' + size);
   }
 
   getUsersByPurchaseDetailAsc(page: number, size: number): Observable<IUserPage> {
-    return this.http.get<IUserPage>(this.url + '/purchaseDetailAsc?page=' + page + '&size=' + size);
+    return this.oHttpClient.get<IUserPage>(this.url + '/purchaseDetailAsc?page=' + page + '&size=' + size);
   }
 
   createUser(user: IUser): Observable<IUser> {
-    return this.http.post<IUser>(this.url, user);
+    return this.oHttpClient.post<IUser>(this.url, user);
   }
 
   generateUsers(amount: number): Observable<number> {
-    return this.http.post<number>(this.url + '/populate/' + amount, {});
+    return this.oHttpClient.post<number>(this.url + '/populate/' + amount, {});
   }
 
   updateUser(user: IUser): Observable<IUser> {
-    return this.http.put<IUser>(this.url, user);
+    return this.oHttpClient.put<IUser>(this.url, user);
   }
 
   deleteUser(id: number | undefined): Observable<number> {
-    return this.http.delete<number>(this.url + '/' + id);
+    return this.oHttpClient.delete<number>(this.url + '/' + id);
   }
 
   deleteAllUsers(): Observable<number> {
-    return this.http.delete<number>(this.url + '/empty');
+    return this.oHttpClient.delete<number>(this.url + '/empty');
   }
 
 }
