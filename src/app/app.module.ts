9f77c5f26c5534c6d80732273ae325cd8f51c6aa
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-01 19:35:45 +0100

    Importaciones.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { TableModule } from 'primeng/table';
-import { PaginatorModule } from 'primeng/paginator';
-import { ConfirmPopupModule } from 'primeng/confirmpopup';
-import { CalendarModule } from 'primeng/calendar';
-import { TooltipModule } from 'primeng/tooltip';
-import { DropdownModule } from 'primeng/dropdown';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
-// --
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// --
-// --
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { PaginatorModule } from 'primeng/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 // --
-// INTERCEPTORS
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+// INTERCEPTOR
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
 // --
 // SHARED COMPONENTS
@@ -50,47 +33,47 @@ import { PurchaseDetailAjaxService } from './service/purchaseDetail.ajax.service
 import { SessionAjaxService } from './service/session.ajax.service';
 import { UserAjaxService } from './service/user.ajax.service';
 // --
-
+// USER COMPONENTS
+import { AdminUserFormUnroutedComponent } from './components/user/admin-user-form-unrouted/admin-user-form-unrouted.component';
+import { AdminUserPlistRoutedComponent } from './components/user/admin-user-plist-routed/admin-user-plist-routed.component'; 
+import { AdminUserPlistUnroutedComponent } from './components/user/admin-user-plist-unrouted/admin-user-plist-unrouted.component'; 
+import { AdminUserDetailUnroutedComponent } from './components/user/admin-user-detail-unrouted/admin-user-detail-unrouted.component'; 
+import { AdminUserNewRoutedComponent } from './components/user/admin-user-new-routed/admin-user-new-routed.component'; 
+import { AdminUserEditRoutedComponent } from './components/user/admin-user-edit-routed/admin-user-edit-routed.component';
+import { AdminUserSelectionUnroutedComponentComponent } from './components/user/admin-user-selection-unrouted-component/admin-user-selection-unrouted-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //--
 @NgModule({
   declarations: [
     AppComponent,
-    //SHARED COMPONENTS
+    // SHARED COMPONENTS
     FooterUnroutedComponent,
     HomeRoutedComponent,
     LoginRoutedComponent,
     LogoutRoutedComponent,
     MenuUnroutedComponent,
+    // USER COMPONENTS
+    AdminUserFormUnroutedComponent,
+    AdminUserPlistRoutedComponent,
+    AdminUserPlistUnroutedComponent,
+    AdminUserDetailUnroutedComponent,
+    AdminUserNewRoutedComponent,
+    AdminUserEditRoutedComponent,
+    AdminUserSelectionUnroutedComponentComponent
   ],
   imports: [
-    RouterModule,
-    //--
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserModule,
-    FormsModule,
+    CommonModule,
     ReactiveFormsModule,
-    //--
-    BrowserAnimationsModule,
     DynamicDialogModule,
     BrowserAnimationsModule,
+    MatSnackBarModule,
     PaginatorModule,
-    TableModule,
-    ConfirmDialogModule,
-    ConfirmPopupModule,
-    CalendarModule,
-    TooltipModule,
-    DropdownModule,
-    //--
-    MatButtonModule,
-    MatCardModule,
-    MatInputModule,
-    MatRadioModule,
-    MatFormFieldModule,
-    MatToolbarModule,
-    MatIconModule,
+    MatProgressSpinnerModule,
+    ConfirmPopupModule
   ],
   providers: [
     // SERVICES
