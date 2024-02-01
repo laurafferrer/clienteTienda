4e7a5ae01134e9b904c896438c50304758440ea3
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-01 19:35:16 +0100

    Creado componente.

---
+++ src/app/components/user/admin-user-selection-unrouted-component/admin-user-selection-unrouted-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-user-selection-unrouted-component',
+  templateUrl: './admin-user-selection-unrouted-component.component.html',
+  styleUrls: ['./admin-user-selection-unrouted-component.component.css']
+})
+export class AdminUserSelectionUnroutedComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
