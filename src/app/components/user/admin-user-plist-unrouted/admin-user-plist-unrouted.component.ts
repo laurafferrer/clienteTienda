6eebbe0ce1dbb3650d831d362810c8e523b538c2
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-01 20:18:52 +0100

    Plist unrouted hecho.

--- src/app/components/user/admin-user-plist-unrouted/admin-user-plist-unrouted.component.ts
+++ src/app/components/user/admin-user-plist-unrouted/admin-user-plist-unrouted.component.ts
@@ -1,7 +1,14 @@
-/*import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
 import { UserAjaxService } from '../../../service/user.ajax.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { IUser, IUserPage } from '../../../model/model.interfaces';
+import { Subject, of } from 'rxjs';
+import { PaginatorState } from 'primeng/paginator';
+import { ConfirmEventType, ConfirmationService } from 'primeng/api';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { AdminUserDetailUnroutedComponent } from '../admin-user-detail-unrouted/admin-user-detail-unrouted.component';
+import { debounceTime, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-user-plist-unrouted',
@@ -10,23 +17,115 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class AdminUserPlistUnroutedComponent implements OnInit {
 
-  @Input() forceReload: Subject<boolena> = new Subject<boolean>();
+  @Input() forceReload: Subject<boolean> = new Subject<boolean>();
 
   oPage: IUserPage | undefined;
-  OrderField: string = 'id';
-  orderDirection: string = 'asc';
-  oPaginatorState: PaginatorState = { first: 0, rows: 10, pageCount: 0 };
+  oOrderField: string = 'id';
+  oOrderDirection: string = 'asc';
+  oPaginatorState: PaginatorState = { first: 0, rows: 10, page: 0, pageCount: 0 };
   status: HttpErrorResponse | null = null;
   oUserToRemove: IUser | null = null; 
 
   constructor(    
     private oUserAjaxService: UserAjaxService,
+    public oDialogService: DialogService,
     private oConfirmationService: ConfirmationService,
     private oMatSnackBar: MatSnackBar,
-    private oUserPrintAjaxService: UserPrintAjaxService
   ) {}
 
   ngOnInit() {
+    this.getPage();
+    this.forceReload.subscribe({
+      next: (v) => {
+        if (v) {
+          this.getPage();
+        }
+      }
+    });
   }
 
-}*/
+  search(filterValue: string): void {
+    if (filterValue && filterValue.length >= 3) {
+      this.oUserAjaxService.getUserPage(this.oPaginatorState.rows, this.oPaginatorState.first, 'id', 'asc', filterValue)
+      .pipe(
+        debounceTime(500),
+        switchMap((data: IUserPage) => {
+          return of(data);
+        })
+      )
+      .subscribe(
+        (data: IUserPage) => {
+          this.oPage = data;
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
+    } else {
+      this.oUserAjaxService.getUserPage(this.oPaginatorState.rows, this.oPaginatorState.first, 'id', 'asc')
+      .subscribe(
+        (data: IUserPage ) => {
+          this.oPage = data;
+        },
+        (error: any) => {
+          console.error(error);
+        }
+      );
+    }
+  }
+
+  getPage(): void {
+    this.oUserAjaxService.getUserPage(this.oPaginatorState.rows, this.oPaginatorState.page, this.oOrderField, this.oOrderDirection).subscribe({
+      next: (data: IUserPage) => {
+        this.oPage = data;
+        this.oPaginatorState.pageCount = data.totalPages;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.status = error;
+      }
+    });
+  }
+
+  onPageChange(event: PaginatorState) {
+    this.oPaginatorState.rows = event.rows;
+    this.oPaginatorState.page = event.page;
+    this.getPage();
+  }
+
+  doOrder(fieldOrder: string){
+    this.oOrderField = fieldOrder;
+    this.oOrderDirection = this.oOrderDirection == "asc" ? "desc" : "asc";
+    this.getPage();
+  }
+
+  doView(user: IUser) {
+    let ref: DynamicDialogRef | undefined;
+    ref = this.oDialogService.open(AdminUserDetailUnroutedComponent, {
+      header: 'User Detail',
+      width: '70%',
+      maximizable: false,
+      data: { id: user.id, ref}
+    });
+  }
+
+  doRemove(user: IUser) {
+    this.oUserToRemove = user;
+    this.oConfirmationService.confirm({
+      accept: () => {
+        this.oMatSnackBar.open("User has been deleted", "Close", {duration: 3000});
+        this.oUserAjaxService.deleteUser(user.id).subscribe({
+          next: () => {
+            this.getPage();
+          },
+          error: (error: HttpErrorResponse) => {
+            this.status = error;
+          }
+        });
+      },
+      reject: (type: ConfirmEventType) => {
+        this.oMatSnackBar.open("User has not been deleted", "Accept", {duration: 3000});
+      }
+    })
+  }
+
+}
