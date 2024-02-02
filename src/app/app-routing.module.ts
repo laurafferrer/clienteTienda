3371edb37efe3460e7bfcf961e5db3fc47d77ce4
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-02 18:22:57 +0100

    Puestas routes de product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeRoutedComponent } from './components/shared/home-routed/home-routed.component';
 import { LoginRoutedComponent } from './components/shared/login-routed/login-routed.component';
 import { LogoutRoutedComponent } from './components/shared/logout-routed/logout-routed.component';
-// -- ADMIN COMPONENTS
+// -- ADMIN USER COMPONENTS
 import { AdminUserPlistRoutedComponent } from './components/user/admin-user-plist-routed/admin-user-plist-routed.component';
 import { AdminUserEditRoutedComponent } from './components/user/admin-user-edit-routed/admin-user-edit-routed.component';
 import { AdminUserNewRoutedComponent } from './components/user/admin-user-new-routed/admin-user-new-routed.component';
-
+// -- ADMIN PRODUCT COMPONENTS
+import { AdminProductPlistRoutedComponent } from './components/product/admin-product-plist-routed/admin-product-plist-routed.component';
+import { AdminProductEditRoutedComponent } from './components/product/admin-product-edit-routed/admin-product-edit-routed.component';
+import { AdminProductNewRoutedComponent } from './components/product/admin-product-new-routed/admin-product-new-routed.component';
 
 
 const routes: Routes = [
@@ -20,6 +23,10 @@ const routes: Routes = [
   { path: 'admin/user/plist', component: AdminUserPlistRoutedComponent},
   { path: 'admin/user/edit/:id', component: AdminUserEditRoutedComponent},
   { path: 'admin/user/new', component: AdminUserNewRoutedComponent},
+
+  { path: 'admin/product/plist', component: AdminProductPlistRoutedComponent},
+  { path: 'admin/product/edit/:id', component: AdminProductEditRoutedComponent},
+  { path: 'admin/product/new', component: AdminProductNewRoutedComponent},
 ];
 
 @NgModule({
