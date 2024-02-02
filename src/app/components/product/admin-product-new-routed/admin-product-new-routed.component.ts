0acd0a73b63adceb4fe271919c5fee80d63c1c84
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-02 18:23:16 +0100

    Cambio de nombre

---
+++ src/app/components/product/admin-product-new-routed/admin-product-new-routed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-product-new-routeed',
+  templateUrl: './admin-product-new-routed.component.html',
+  styleUrls: ['./admin-product-new-routed.component.css']
+})
+export class AdminProductNewRoutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
