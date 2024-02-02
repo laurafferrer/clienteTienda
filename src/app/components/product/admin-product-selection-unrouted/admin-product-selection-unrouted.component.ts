1c99a85cb4f9c7ae8c62fe71430aeed324cc394e
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-02 19:33:34 +0100

    Component admin product selection hecho.

--- src/app/components/product/admin-product-selection-unrouted/admin-product-selection-unrouted.component.ts
+++ src/app/components/product/admin-product-selection-unrouted/admin-product-selection-unrouted.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct, IProductPage } from '../../../model/model.interfaces';
+import { PaginatorState } from 'primeng/paginator';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ProductAjaxService } from '../../../service/product.ajax.service';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-admin-product-selection-unrouted',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductSelectionUnroutedComponent implements OnInit {
 
-  constructor() { }
+  oPage: IProductPage | undefined;
+  oOrderFiled: string = "id";
+  oOrderDirection: string = "asc";
+  oPaginatorState: PaginatorState = { first: 0, rows: 10, page: 0, pageCount: 0 };
+  oStatus: HttpErrorResponse | null = null;
+
+  constructor(
+    private oProductAjaxService: ProductAjaxService,
+    private oDialogService: DialogService,
+    private oDynamicDialogRef: DynamicDialogRef
+  ) { }
 
   ngOnInit() {
+    this.getPage();
+  }
+
+  getPage(): void {
+    this.oProductAjaxService.getPageProducts(this.oPaginatorState.rows, this.oPaginatorState.page, this.oOrderFiled, this.oOrderDirection).subscribe({
+      next: (data: IProductPage) => {
+        this.oPage = data;
+        this.oPaginatorState.pageCount = data.totalPages;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.oStatus = error;
+      }
+    });
+  }
+
+  onPageChange(event: PaginatorState) {
+    this.oPaginatorState.rows = event.rows;
+    this.oPaginatorState.page = event.page;
+    this.getPage();
+  }
+
+  doOrder(fieldOrder: string) {
+    this.oOrderFiled = fieldOrder;
+    if (this.oOrderDirection == "asc") {
+      this.oOrderDirection = "desc";
+    } else {
+      this.oOrderDirection = "asc";
+    }
+    this.getPage();
+  }
+
+  selectProduct(product: IProduct) {
+    this.oDynamicDialogRef.close(product);
   }
 
 }
