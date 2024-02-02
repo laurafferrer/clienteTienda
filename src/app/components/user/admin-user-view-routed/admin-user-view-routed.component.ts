5e3428c65f6aa06c72e2831cc3824e3003e220fe
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-02 19:42:18 +0100

    Component admin user view routed creado.

---
+++ src/app/components/user/admin-user-view-routed/admin-user-view-routed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-user-view-routed',
+  templateUrl: './admin-user-view-routed.component.html',
+  styleUrls: ['./admin-user-view-routed.component.css']
+})
+export class AdminUserViewRoutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
