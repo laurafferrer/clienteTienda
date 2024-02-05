f6b293b0e6cda2eba30e80c03e9870571b8f9d4a
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-05 17:20:27 +0100

    Component admin category detail unrouted hecho.

--- src/app/components/category/admin-category-detail-unrouted/admin-category-detail-unrouted.component.ts
+++ src/app/components/category/admin-category-detail-unrouted/admin-category-detail-unrouted.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Optional } from '@angular/core';
+import { ICategory } from '../../../model/model.interfaces';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CategoryAjaxService } from '../../../service/category.ajax.service';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-admin-category-detail-unrouted',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCategoryDetailUnroutedComponent implements OnInit {
 
-  constructor() { }
+  @Input() id: number = 1;
+
+  oCategory: ICategory = {} as ICategory;
+  oStatus: HttpErrorResponse | null = null;
+
+  constructor(
+    private oCategoryAjaxService: CategoryAjaxService,
+    @Optional() public ref: DynamicDialogRef,
+    @Optional() public config: DynamicDialogConfig
+  ) {
+    if (config) {
+      if (config.data) {
+        this.id = config.data.id;
+      }
+    }
+   }
 
   ngOnInit() {
+    this.getOne();
+  }
+
+  getOne() {
+    this.oCategoryAjaxService.getCategoryById(this.id).subscribe({
+      next: (data: ICategory) => {
+        this.oCategory = data;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.oStatus = error;
+      }
+    });
   }
 
 }
