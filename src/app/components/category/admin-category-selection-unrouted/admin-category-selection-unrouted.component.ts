91e7233d588d6dd1af4eff878b24933454df99a1
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-05 12:21:25 +0100

    Component admin category selection unrouted hecho.

--- src/app/components/category/admin-category-selection-unrouted/admin-category-selection-unrouted.component.ts
+++ src/app/components/category/admin-category-selection-unrouted/admin-category-selection-unrouted.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ICategory, ICategoryPage } from '../../../model/model.interfaces';
+import { PaginatorState } from 'primeng/paginator';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CategoryAjaxService } from '../../../service/category.ajax.service';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-admin-category-selection-unrouted',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCategorySelectionUnroutedComponent implements OnInit {
 
-  constructor() { }
+  oPage: ICategoryPage | undefined;
+  oOrderField: string = "id";
+  oOrderDirection: string = "asc";
+  oPaginatorState: PaginatorState = { first: 0, rows: 10, page: 0, pageCount: 0 };
+  oStatus: HttpErrorResponse | null = null;
+
+  constructor(
+    private oCategoryAjaxService: CategoryAjaxService,
+    private oDialogService: DialogService,
+    private oDynamicDialogRef: DynamicDialogRef
+  ) { }
 
   ngOnInit() {
+    this.getPage();
+  }
+
+  getPage(): void {
+    this.oCategoryAjaxService.getPageCategories(this.oPaginatorState.rows, this.oPaginatorState.page, this.oOrderField, this.oOrderDirection).subscribe({
+      next: (data: ICategoryPage) => {
+        this.oPage = data;
+        this.oPaginatorState.pageCount = data.totalPages;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.oStatus = error;
+      }
+    });
+  }
+
+  onPageChange(event: PaginatorState) {
+    this.oPaginatorState.rows = event.rows;
+    this.oPaginatorState.page = event.page;
+    this.getPage();
+  }
+
+  doOrder(fieldOrder: string) {
+    this.oOrderField = fieldOrder;
+    if (this.oOrderDirection == "asc") {
+      this.oOrderDirection = "desc";
+    } else {
+      this.oOrderDirection = "asc";
+    }
+    this.getPage();
+  }
+
+  selectCategory(categroy: ICategory) {
+    this.oDynamicDialogRef.close(categroy);
   }
 
 }
