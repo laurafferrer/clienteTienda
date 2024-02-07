a63b0185e59e2ef065b385841f60aebfe64b1703
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-07 19:59:01 +0100

    Creados component purchase.

---
+++ src/app/components/purchase/admin-purchase-detail-unrouted/admin-purchase-detail-unrouted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-purchase-detail-unrouted',
+  templateUrl: './admin-purchase-detail-unrouted.component.html',
+  styleUrls: ['./admin-purchase-detail-unrouted.component.css']
+})
+export class AdminPurchaseDetailUnroutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/purchase/admin-purchase-plist-unrouted/admin-purchase-plist-unrouted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-purchase-plist-unrouted',
+  templateUrl: './admin-purchase-plist-unrouted.component.html',
+  styleUrls: ['./admin-purchase-plist-unrouted.component.css']
+})
+export class AdminPurchasePlistUnroutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
