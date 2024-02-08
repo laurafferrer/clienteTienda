c54bfc6552f56bbc9ae99eaab6ec045f95fc8211
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-08 13:27:39 +0100

    Componetns cart routed creados.

---
+++ src/app/components/cart/admin-cart-plist-routed/admin-cart-plist-routed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-cart-plist-routed',
+  templateUrl: './admin-cart-plist-routed.component.html',
+  styleUrls: ['./admin-cart-plist-routed.component.css']
+})
+export class AdminCartPlistRoutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/cart/admin-cart-view-routed/admin-cart-view-routed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-cart-view-routed',
+  templateUrl: './admin-cart-view-routed.component.html',
+  styleUrls: ['./admin-cart-view-routed.component.css']
+})
+export class AdminCartViewRoutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
