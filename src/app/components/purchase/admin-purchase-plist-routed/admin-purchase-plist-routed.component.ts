186587fb5e7e5fd91e20d962642eb272338d4eac
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-08 13:24:37 +0100

    Components routed purchase creados

---
+++ src/app/components/purchase/admin-purchase-plist-routed/admin-purchase-plist-routed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-purchase-plist-routed',
+  templateUrl: './admin-purchase-plist-routed.component.html',
+  styleUrls: ['./admin-purchase-plist-routed.component.css']
+})
+export class AdminPurchasePlistRoutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/purchase/admin-purchase-view-routed/admin-purchase-view-routed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-purchase-view-routed',
+  templateUrl: './admin-purchase-view-routed.component.html',
+  styleUrls: ['./admin-purchase-view-routed.component.css']
+})
+export class AdminPurchaseViewRoutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
