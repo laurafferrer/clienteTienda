2bf3f6e8fc068267085891d0a8f366bbbe2e56a2
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-08 12:04:55 +0100

    Rename

---
+++ src/app/components/purchaseDetail/admin-purchaseDetail-plist-unrouted/admin-purchaseDetail-plist-unrouted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-purchaseDetail-plist-unrouted',
+  templateUrl: './admin-purchaseDetail-plist-unrouted.component.html',
+  styleUrls: ['./admin-purchaseDetail-plist-unrouted.component.css']
+})
+export class AdminPurchaseDetailPlistUnroutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
