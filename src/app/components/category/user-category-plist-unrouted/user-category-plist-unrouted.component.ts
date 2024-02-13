0edb18d5920759c6163a265f783b1d9c020e8066
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-13 10:36:15 +0100

    Component user category plist unrouted hecho

--- src/app/components/category/user-category-plist-unrouted/user-category-plist-unrouted.component.ts
+++ src/app/components/category/user-category-plist-unrouted/user-category-plist-unrouted.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ICategoryPage } from '../../../model/model.interfaces';
+import { PaginatorState } from 'primeng/paginator';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CategoryAjaxService } from '../../../service/category.ajax.service';
 
 @Component({
   selector: 'app-user-category-plist-unrouted',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCategoryPlistUnroutedComponent implements OnInit {
 
-  constructor() { }
+  @Input() oForceReload: Subject<boolean> = new Subject<boolean>();
+  
+  oPage: ICategoryPage | undefined;
+  oOrderField: string = "id";
+  oOrderDirection: string = "asc";
+  oPaginatorState: PaginatorState = { first: 0, rows: 10, page: 0, pageCount: 0 };
+  oStatus: HttpErrorResponse | null = null;
+
+  constructor(
+    private oCategoryAjaxService: CategoryAjaxService
+    ) { }
 
   ngOnInit() {
+    this.getCategories();
+    this.oForceReload.subscribe({
+      next: (v) => {
+        if (v) {
+          this.getCategories();
+        }
+      }
+    })
+  }
+
+  getCategories(): void {
+    this.oCategoryAjaxService.getPageCategory(this.oPaginatorState.rows ?? 0, this.oPaginatorState.page ?? 0, this.oOrderField, this.oOrderDirection).subscribe({
+      next: (data: ICategoryPage) => {
+        this.oPage = data;
+        this.oPaginatorState.pageCount = data.totalPages;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.oStatus = error;
+      }
+    })
+  }
+
+  onPageChange(event: PaginatorState) {
+    this.oPaginatorState.rows = event.rows;
+    this.oPaginatorState.page = event.page;
+    this.getCategories();
   }
 
 }
