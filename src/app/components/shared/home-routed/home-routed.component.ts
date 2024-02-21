19394f0f218fe52350e04ca418bb57580a0119aa
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-21 18:56:37 +0100

    Productos en el home.

--- src/app/components/shared/home-routed/home-routed.component.ts
+++ src/app/components/shared/home-routed/home-routed.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from '../../../model/model.interfaces';
+import { NavigationEnd, Router } from '@angular/router';
+import { SessionAjaxService } from '../../../service/session.ajax.service';
+import { UserAjaxService } from '../../../service/user.ajax.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-home-routed',
@@ -7,7 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeRoutedComponent implements OnInit {
 
-  constructor() { }
+  oUsername: string = '';
+  oUserSession: IUser | null = null;
+  oUrl: string = '';
+
+  constructor(
+    private oRouter: Router,
+    private oSessionService: SessionAjaxService,
+    private oUserAjaxService: UserAjaxService
+  ) { 
+    this.oRouter.events.subscribe((ev) => {
+      if (ev instanceof NavigationEnd) {
+        this.oUrl = ev.url;
+      }
+    })
+
+    this.oUsername = oSessionService.getUsername();
+    this.oUserAjaxService.getUserByUsername(this.oSessionService.getUsername()).subscribe({
+      next: (user: IUser) => {
+        this.oUserSession = user;
+        console.log('User Session:', this.oUserSession); // Agrega este log
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      }
+    });
+  }
 
   ngOnInit() {
   }
